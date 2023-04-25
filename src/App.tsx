f8d27603cb82c946b681edaa8aaf4d0c1e64dcd6
Denis <denis140292@gmail.com>
2023-04-25 21:04:42 +0300

    feat(Todolist) created Todolist.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import './App.css';
+import Todolist from "./components/Todolist";
+import {v1} from 'uuid';
 
 function App() {
+  const tasks: Array<taskType> = [
+    {id: v1(), title: 'HTML', isDone: true},
+    {id: v1(), title: 'CSS', isDone: true},
+    {id: v1(), title: 'JavaScript', isDone: false},
+  ]
   return (
     <div className="App">
-      Hello World!
+      <Todolist title={'What to learn'} tasks={tasks}/>
     </div>
   );
 }
 
 export default App;
+
+
+export type taskType = {
+  id: string,
+  title: string,
+  isDone: boolean
+}
